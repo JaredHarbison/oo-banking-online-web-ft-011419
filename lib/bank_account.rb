cf016f87d35761227b20cea946d56d78e42c13b1
Jared Harbison <jared.harbison@gmail.com>
2019-01-31 15:32:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,9 @@
 class BankAccount
-  attr_accessor :name, :balance, :status 
+  attr_reader :name 
+  attr_accessor :status 
 
   def initialize(name)
     @name = name 
-    @balance = 1000
     @status = "open"
   end 
 
